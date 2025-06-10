f9254ddc8a51e15bb1bf1f4e9cd74aab62630370
shreyasharma003 <shreyusharma11@gmail.com>
2025-06-10 12:53:35 +0530

    Updated some codes

---
+++ README.md
@@ -0,0 +1,17 @@
+# To-Do List Web App ✅📝
+
+A simple and interactive To-Do List web application built using **HTML**, **CSS**, and **JavaScript**.
+
+## 🚀 Features
+- ➕ Add new tasks
+- ✅ Priority wise tasks
+- 🗑️ Delete tasks
+- 💾 Data stored temporarily (can be extended with localStorage)
+
+## 🛠️ Technologies Used
+- HTML5
+- CSS3
+- JavaScript (Vanilla)
+
+
+
--- index.js
+++ index.js
@@ -1,68 +1,3 @@
-// document.querySelector("form").addEventListener("submit", myTodo);
-
-// let todoArr;
-// if (localStorage.getItem("todoArr") == null) {
-//     todoArr = []
-// }
-// else {
-//    todoArr =  JSON.parse(localStorage.getItem("todoArr"))
-// }
-// displayTable(todoArr)
-
-// function myTodo() {
-//   event.preventDefault();
-//   var task = document.querySelector("#task").value;
-//   var priority = document.querySelector("#priority").value;
-
-//   let todoObj = {
-//     task,
-//     priority,
-//   };
-//     todoArr.push(todoObj);
-//     localStorage.setItem("todoArr",JSON.stringify(todoArr));
-
-//   displayTable(todoArr);
-// }
-
-// function displayTable(todoArr) {
-//   document.querySelector("tbody").innerHTML = "";
-//   todoArr.map(function (elem,index) {
-//     var tr = document.createElement("tr");
-//     var td1 = document.createElement("td");
-//     td1.innerText = elem.task;
-
-//     var td2 = document.createElement("td");
-//     td2.innerText = elem.priority;
-
-//     if (elem.priority == "High") {
-//       td2.style.backgroundColor = "red";
-//     } else {
-//       td2.style.backgroundColor = "green";
-//     }
-
-//       var td3 = document.createElement("td");
-//       td3.addEventListener("click", function(){
-//           //  event.target.parentNode.remove();
-//           rowremove(elem,index);
-//        });
-//     td3.innerText = "Delete";
-//     td3.style.backgroundColor = "red";
-
-//     tr.append(td1, td2, td3);
-//     document.querySelector("tbody").append(tr);
-//   });
-
-// }
-
-// function rowremove(elem, index) {
-//     todoArr.splice(index, 1)
-//     localStorage.setItem("todoArr",JSON.stringify(todoArr));
-//     displayTable(todoArr)
-
-// }
-
-    
-
 let form = document.querySelector("form");
 form.addEventListener("submit", datafun);
 
@@ -80,7 +15,7 @@ function datafun() {
 
     arr.push(data_obj);
     display(arr);
-  // console.log(arr);
+  
 }
 
 
@@ -95,9 +30,9 @@ function display(data) {
     let col2 = document.createElement("td");
     col2.innerText = el.preority;
     if (el.preority == "High") {
-      col2.style.backgroundColor = "red";
+      col2.style.backgroundColor = "orange";
     } else {
-      col2.style.backgroundColor = "green";
+      col2.style.backgroundColor = "cyan";
     }
 
       let col3 = document.createElement("td");
@@ -119,8 +54,5 @@ function del(index) {
 }
 
 
-// let ar = [1, 2, 3, 4, 5]
-// ar.splice(1, 2)
-// console.log(ar);
 
 
